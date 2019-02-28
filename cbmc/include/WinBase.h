e1fd50655fecccd5c718ffbf558979ba41a2bc03
Mark R. Tuttle <mrtuttle@amazon.com>
2019-02-28 11:57:45 -0500

    Deleted contents of Windows.h and WinBase.h

--- cbmc/include/WinBase.h
+++
@@ -1,5 +0,0 @@
-#if 0
-WinBase.h is included by much of the FreeRTOS code, so we need to have this file
-around when we build harnesses. Having an empty file is fine, as the harnesses
-don't use anything from WinBase.h.
-#endif
