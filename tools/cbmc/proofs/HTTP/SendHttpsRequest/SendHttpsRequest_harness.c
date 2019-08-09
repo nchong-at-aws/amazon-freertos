2de8a51cfd109b256fe1c79a6eff0e00979bfc63
Mark R. Tuttle <mrtuttle@amazon.com>
2019-08-09 16:38:18 -0400

    Improve coverage of SendHttpsRequest proof

--- tools/cbmc/proofs/HTTP/SendHttpsRequest/SendHttpsRequest_harness.c
+++ tools/cbmc/proofs/HTTP/SendHttpsRequest/SendHttpsRequest_harness.c
@@ -14,6 +14,46 @@ void _sendHttpsRequest( IotTaskPool_t pTaskPool,
 			IotTaskPoolJob_t pJob,
 			void * pUserContext );
 
+/* This is a clang macro not available on linux */
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
+#if __has_builtin(__builtin___memcpy_chk)
+void *__builtin___memcpy_chk(void *dest, const void *src, size_t n, size_t m) {
+  __CPROVER_assert(__CPROVER_w_ok(dest, n), "write");
+  __CPROVER_assert(__CPROVER_r_ok(src, n), "read");
+  return dest;
+}
+#else
+void *memcpy(void *dest, const void *src, size_t n) {
+  __CPROVER_assert(__CPROVER_w_ok(dest, n), "write");
+  __CPROVER_assert(__CPROVER_r_ok(src, n), "read");
+  return dest;
+}
+#endif
+
+#if __has_builtin(__builtin___sprintf_chk)
+int __builtin___snprintf_chk (char *buf, size_t size, int flag, size_t os,
+			      const char *fmt, ...)
+{
+  int ret;
+  __CPROVER_assert(__CPROVER_w_ok(buf, size), "sprintf output writeable");
+  __CPROVER_assert(fmt, "sprintf format nonnull");
+  __CPROVER_assume(ret >= 0 && ret <= HTTPS_MAX_CONTENT_LENGTH_LINE_LENGTH);
+  return ret;
+}
+#else
+int snprintf(char *buf, size_t size, const char *fmt, ...)
+{
+  int ret;
+  __CPROVER_assert(__CPROVER_w_ok(buf, size), "sprintf output writeable");
+  __CPROVER_assert(fmt, "sprintf format nonnull");
+  __CPROVER_assume(ret >= 0 && ret <= HTTPS_MAX_CONTENT_LENGTH_LINE_LENGTH);
+  return ret;
+}
+#endif
+
 void harness() {
   IotTaskPool_t pTaskPool;
   IotTaskPoolJob_t pJob;
@@ -26,11 +66,15 @@ void harness() {
   initialize_IotConnectionHandle(reqHandle->pHttpsConnection);
   initialize_IotResponseHandle(reqHandle->pHttpsResponse);
 
-  // ???: Sarena: this section is highly questionable!
   IotHttpsResponseHandle_t respHandle1 = allocate_IotResponseHandle();
   __CPROVER_assume(respHandle1);
   initialize_IotResponseHandle(respHandle1);
-  IotListDouble_InsertHead(&reqHandle->pHttpsResponse->link, &respHandle1->link);
+  IotListDouble_InsertHead(&reqHandle->pHttpsConnection->respQ, &respHandle1->link);
+
+  IotHttpsRequestHandle_t reqHandle1 = allocate_IotRequestHandle();
+  __CPROVER_assume(reqHandle1);
+  initialize_IotRequestHandle(reqHandle1);
+  IotListDouble_InsertHead(&reqHandle->pHttpsConnection->reqQ, &reqHandle1->link);
 
   __CPROVER_assume(is_stubbed_IotRequestHandle(reqHandle));
   __CPROVER_assume(is_valid_IotRequestHandle(reqHandle));
